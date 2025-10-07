a54ad590317e2298511fdb65d265ea44a1ce7bd1
qbaucellsd <146918473+qbaucellsd@users.noreply.github.com>
2025-10-07 16:08:18 +0200

    Commit7
    [COMMIT] Moviment jugador.

--- ex-5.dodge-the-creep/player.gd
+++ ex-5.dodge-the-creep/player.gd
@@ -10,4 +10,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var velocity = Vector2.ZERO # Vector de moviment del jugador.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
