e907de9c20df358f8587caee18532feee6dfbe91
qbaucellsd <146918473+qbaucellsd@users.noreply.github.com>
2025-10-07 16:04:18 +0200

    Commit6
    [COMMIT] Assignació de tecles.

--- ex-5.dodge-the-creep/player.gd
+++ ex-5.dodge-the-creep/player.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
+@export var speed: float = 400.0 # A quina velocitat es mourà el jugador (píxels/seg)
+var screen_size: Vector2 # Mida de la finestra de joc
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
