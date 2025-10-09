ae6e2e7bc6e73543e5f69bba4a8023fa1467ae44
qbaucellsd <146918473+qbaucellsd@users.noreply.github.com>
2025-10-09 16:03:28 +0200

    Commit12
    [COMMIT] Escena Mob acabada.

---
+++ ex-5.dodge-the-creep/mob.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$AnimatedSprite2D.playing = true
+	var mob_types = $AnimatedSprite2D.frames.get_animation_names()
+	$AnimatedSprite2D.animation = mob_types[randi() % mob_types.size()]
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
