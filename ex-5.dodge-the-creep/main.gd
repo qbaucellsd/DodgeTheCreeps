556d20f7820ca35c03e8113d4b37baad73ee2de9
qbaucellsd <146918473+qbaucellsd@users.noreply.github.com>
2025-10-14 15:55:52 +0200

    Commit15
    [COMMIT] Main Script.

---
+++ ex-5.dodge-the-creep/main.gd
@@ -0,0 +1,47 @@
+extends Node2D
+
+@export var mob_scene: PackedScene
+var score: int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	randomize()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func game_over() -> void:
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+func _on_score_timer_timeout() -> void:
+	score += 1
+
+func _on_start_timer_timeout() -> void:
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+func _on_mob_timer_timeout() -> void:
+	# Crea una nova instància de l'escena Mob.
+	var mob = mob_scene.instantiate()
+	# Trieu una ubicació aleatòria a Path2D.
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.progress_ratio = randf()  # offset → progress_ratio en Godot 4
+	# Estableix la direcció de la multitud perpendicular a la direcció del camí.
+	var direction = mob_spawn_location.rotation + PI / 2
+	# Estableix la posició de la multitud a una ubicació aleatòria.
+	mob.position = mob_spawn_location.position
+	# Afegeix una mica d'aleatorietat a la direcció.
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	# Tria la velocitat de la multitud.
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	# Afegeix la multitud a l'escena principal.
+	add_child(mob)
